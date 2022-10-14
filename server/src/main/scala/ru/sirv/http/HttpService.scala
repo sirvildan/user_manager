cb6687760ef5e5402ad1cf298e890d2d5f06101e
vildan.sirbaev <sirvildan@gmail.com>
2022-10-14 18:56:46 +0500

    implement http service, add rest tests

--- server/src/main/scala/ru/sirv/http/HttpService.scala
+++ server/src/main/scala/ru/sirv/http/HttpService.scala
@@ -5,10 +5,11 @@ import cats.syntax.all._
 import com.typesafe.scalalogging.Logger
 import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
-import org.http4s.dsl.impl.IntVar
+import org.http4s.circe.toMessageSyntax
 import org.http4s.dsl.io._
 import org.http4s.server.Router
 import ru.sirv.service._
+import ru.sirv.domain._
 
 class HttpService(service: UserService)(implicit logger: Logger){
   def helloWorldService: HttpRoutes[IO] = HttpRoutes.of[IO] {
@@ -17,21 +18,16 @@ class HttpService(service: UserService)(implicit logger: Logger){
       Ok(s"Hello, $name.")
   }
   def userService: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root / "users" => Ok("GET TEST")
-      //service.getUser(_).onError { e =>
-      //  logger.error(e.getMessage).pure[IO]
-      //}.flatMap(Ok(_))
-    case GET -> Root / "users" / IntVar(userId) => Ok(s"GET by id=$userId")
+    case GET -> Root / "users" =>
+      service.getAllUsers.flatMap(Ok(_))
+    case GET -> Root / "users" / UUIDVar(userId) =>
       service.getUser(userId).flatMap(Ok(_))
-    case GET -> Root / "test" =>
-      Ok("testing message")
-    case POST -> Root / "users" => Ok("POST TEST")
-      //req.decodeJson[Tweet].flatMap(tweet => Ok(tweet))
-    case PUT -> Root / "users" / IntVar(userId) => Ok(s"PUT by id=$userId")
-      //logger.info(userId.toString)
-      //req.decodeJson[Tweet].flatMap(tweet => Ok(tweet))
-    case DELETE -> Root / "users" / IntVar(userId) =>
-      Ok(s"DELETE TEST id=$userId")
+    case req@POST -> Root / "users" =>
+      req.decodeJson[User].flatMap(user => service.addUser(user)) *> Created()
+    case req@PUT -> Root / "users" / UUIDVar(userId) =>
+      req.decodeJson[User].flatMap(user => service.updateUser(user, userId)) *> Ok()
+    case DELETE -> Root / "users" / UUIDVar(userId) =>
+      service.deleteUser(userId) *> Ok()
   }
   def services = userService <+> helloWorldService
   def httpApp = Router("/" -> helloWorldService, "/api" -> services).orNotFound
--- server/src/main/scala/ru/sirv/service/service.scala
+++ server/src/main/scala/ru/sirv/service/service.scala
@@ -1,19 +1,33 @@
 package ru.sirv.service
 
 import cats.effect.IO
+import cats.implicits.catsSyntaxApplicativeId
 import com.typesafe.scalalogging.Logger
 import ru.sirv.domain._
 
+import java.util.UUID
+
 class UserService{
   implicit val logger = Logger("UserService")
-  def getUser(userId: Int): IO[User] = {
+  def getUser(userId: UUID): IO[User] = {
     logger.info(s"Get user by id $userId")
-    IO.delay(User("ss@mal.ru", "mock", None))
-
+    IO.pure(User("ss@mal.ru", "mock", None))
+  }
+  def getAllUsers: IO[Seq[User]] = {
+    logger.info("Get all users")
+    Seq(User("mail1@k","Name1",12), User("mail2@k","Name2",14)).pure[IO]
   }
 
-  //  def getPopularTweets(): IO[Seq[Tweet]] =
-//    Seq(Tweet(1, "first"), Tweet(2, "second"), Tweet(3, "third")).pure[IO]
-
-//  def saveTweet(): IO[Unit] = IO.println("Save tweet test").onError { e => logger.error(e.getMessage).pure[IO] }
+  def deleteUser(userId: UUID): IO[User] = {
+    logger.info(s"Delete user by $userId")
+    IO.pure(User("ss@mal.ru", "mock", None))
+  }
+  def addUser(User: User): IO[UUID] = {
+    logger.info(s"Add user $User")
+    IO.pure(UUID.randomUUID())
+  }
+  def updateUser(User: User, userId: UUID): IO[Unit] = {
+    logger.info(s"Update user $User id=$userId")
+    IO.unit
+  }
 }
